34a6e59390858c226b565a959067915196832771
vicenrele <vicenrele@gmail.com>
2025-06-22 17:37:08 +0200

    chore: reset proyect to only scaffolding files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,3 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  {
-    path: 'library',
-    loadChildren: () =>
-      import('./library/library.module').then(m => m.LibraryModule)
-  },
-  { path: '', redirectTo: '/library', pathMatch: 'full' },
-  { path: '**', redirectTo: '/library' }
-];
+export const routes: Routes = [];
--- src/app/core/models/book.model.ts
+++
@@ -1,7 +0,0 @@
-export interface IBook {
-  id: number;
-  title: string;
-  author: string;
-  year: number;
-  genre: string;
-}
