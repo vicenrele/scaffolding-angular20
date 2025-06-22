97ccfc26da1eea710444531d8799c06acfd32e85
vicenrele <vicenrele@gmail.com>
2025-06-22 19:50:41 +0200

    chore: generate book-detail component

---
+++ src/app/library/components/book-detail/book-detail-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-detail-component',
+  imports: [],
+  templateUrl: './book-detail-component.html',
+  styleUrl: './book-detail-component.scss'
+})
+export class BookDetailComponent {
+
+}
