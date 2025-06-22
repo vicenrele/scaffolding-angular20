6540a009dfd0631b71c1a65b550cbca830fb5677
vicenrele <vicenrele@gmail.com>
2025-06-22 18:36:20 +0200

    chore: clean code

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,16 +1,10 @@
 import { NgModule, Optional, SkipSelf, ModuleWithProviders } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
 import { IBookService } from './services/ibook.service';
 import { InMemoryBookService } from './services/in-memory-book.service';
 import { HttpBookService } from './services/http-book.service';
-// import { HttpBookService } from './services/http-book.service';
 
 @NgModule({
-  imports: [
-    HttpClientModule
-  ],
   providers: [
-    // Cambia aquí entre InMemoryBookService y HttpBookService según lo necesites
     { provide: IBookService, useClass: InMemoryBookService },
     { provide: IBookService, useClass: HttpBookService }
   ]
--- src/app/library/library.module.ts
+++ src/app/library/library.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LibraryRoutingModule } from './library-routing.module';
-import { IBookService } from '../core/services/ibook.service';
-import { HttpBookService } from '../core/services/http-book.service';
-import { InMemoryBookService } from '../core/services/in-memory-book.service';
+// import { IBookService } from '../core/services/ibook.service';
+// import { HttpBookService } from '../core/services/http-book.service';
+// import { InMemoryBookService } from '../core/services/in-memory-book.service';
+import { CoreModule } from '../core/core.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    LibraryRoutingModule
+    LibraryRoutingModule,
+    CoreModule
   ],
-  providers: [
-    { provide: IBookService, useClass: InMemoryBookService },
-    { provide: IBookService, useClass: HttpBookService }
-  ]  
+  // providers: [
+  //   { provide: IBookService, useClass: InMemoryBookService },
+  //   { provide: IBookService, useClass: HttpBookService }
+  // ]  
 })
 export class LibraryModule {}
