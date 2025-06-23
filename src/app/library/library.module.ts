8026934f8f69cee36a987e30130d03587127c5e5
vicenrele <vicenrele@gmail.com>
2025-06-23 09:32:52 +0200

    fix: clean code

--- src/app/library/library.module.ts
+++ src/app/library/library.module.ts
@@ -3,9 +3,6 @@ import { CommonModule } from '@angular/common';
 import { LibraryRoutingModule } from './library-routing.module';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
-// import { IBookService } from '../core/services/ibook.service';
-// import { HttpBookService } from '../core/services/http-book.service';
-// import { InMemoryBookService } from '../core/services/in-memory-book.service';
 import { CoreModule } from '../core/core.module';
 
 @NgModule({
@@ -15,10 +12,6 @@ import { CoreModule } from '../core/core.module';
     CoreModule.forRoot(false), // Use InMemoryBookService by default
     MatTableModule,
     MatPaginatorModule
-  ],
-  // providers: [
-  //   { provide: IBookService, useClass: InMemoryBookService },
-  //   { provide: IBookService, useClass: HttpBookService }
-  // ]  
+  ]
 })
 export class LibraryModule {}
